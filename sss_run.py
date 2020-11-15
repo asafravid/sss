745e7c93e74f6405c82ed2ca9daa3e5cf4b2e981
Asaf <46109899+asafravid@users.noreply.github.com>
2020-11-15 02:52:21 +0200

    V3
    More details

--- sss_run.py
+++ sss_run.py
@@ -1,5 +1,5 @@
 ######
-# V2 #
+# V3 #
 ######
 
 
@@ -14,15 +14,15 @@ import numpy as np
 
 
 # Research Mode:
-MIN_EVR = 1
-MAX_EVR = 10
-PM_MIN  = 5
+MIN_EVR = 3
+MAX_EVR = 15
+PM_MIN  = 10
 PM_MAX  = 100
 research_rows = np.zeros( (MAX_EVR-MIN_EVR+1, PM_MAX-PM_MIN+1), dtype=int )
 for enterprise_value_to_revenue_limit in range(MIN_EVR,MAX_EVR+1):
     for profit_margin_limit in range(PM_MIN,PM_MAX+1):
         num_results = sss.sss_run(sectors_list=[], build_csv_db_only=0, build_csv_db=0, csv_db_path='Results/20201115-001203_MARKETCAP_FORWARDEPS', read_united_states_input_symbols=0, tase_mode=0, num_threads=1, forward_eps_included=0, market_cap_included=1, use_investpy=0, research_mode=1, profit_margin_limit=float(profit_margin_limit)/100.0, best_n_select=50, enterprise_value_to_revenue_limit=enterprise_value_to_revenue_limit)
-        if num_results == 0: break  # already 0 results. With higher profit margin limit there will still be 0
+        if num_results < 20: break  # already 0 results. With higher profit margin limit there will still be 0
         research_rows[enterprise_value_to_revenue_limit-MIN_EVR][profit_margin_limit-PM_MIN] = int(num_results)
         print('row {:3} -> (enterprise_value_to_revenue_limit {:3}) | col {:3} -> (profit_margin_limit {:3}%): num_results = {}'.format(enterprise_value_to_revenue_limit-MIN_EVR, enterprise_value_to_revenue_limit, profit_margin_limit-PM_MIN, profit_margin_limit, num_results))
-np.savetxt('Results/research_results.csv', research_rows.astype(int), fmt='%d', delimiter=',')
+np.savetxt('Results/research_results_evr{}to{}_pm{}to{}.csv'.format(MIN_EVR,MAX_EVR,PM_MIN,PM_MAX), research_rows.astype(int), fmt='%d', delimiter=',')
