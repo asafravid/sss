bbfe8ffef3df3bb6fede748f797b8ba15d5a7005
Asaf <46109899+asafravid@users.noreply.github.com>
2021-01-17 07:40:53 +0200

    V19
    No duplicate files

--- sss_run.py
+++ sss_run.py
@@ -1,5 +1,5 @@
 #######
-# V18 #
+# V20 #
 #######
 
 
@@ -20,7 +20,7 @@ import os
 
 # Run Build DB Only: All/Others
 # =============================
-# sss.sss_run(sectors_list=[], build_csv_db_only=1, build_csv_db=1, csv_db_path='None', read_united_states_input_symbols=1, tase_mode=0, num_threads=20, market_cap_included=1, use_investpy=0, research_mode=0, profit_margin_limit=0.10, ev_to_cfo_ratio_limit = 100.0, best_n_select=3, enterprise_value_to_revenue_limit=20, generate_result_folders=1)
+sss.sss_run(sectors_list=[], build_csv_db_only=1, build_csv_db=1, csv_db_path='None', read_united_states_input_symbols=1, tase_mode=0, num_threads=20, market_cap_included=1, use_investpy=0, research_mode=0, profit_margin_limit=0.10, ev_to_cfo_ratio_limit = 100.0, best_n_select=3, enterprise_value_to_revenue_limit=20, generate_result_folders=1)
 
 # Run Build DB Only: TASE
 # =============================
@@ -56,13 +56,11 @@ def research_db(min_evr, max_evr, pm_min, pm_max, csv_db_path, read_united_state
             research_rows_ssss[ enterprise_value_to_revenue_limit-min_evr][profit_margin_limit-pm_min] = int(num_results_for_evr_and_pm)
             research_rows_sssss[enterprise_value_to_revenue_limit-min_evr][profit_margin_limit-pm_min] = int(num_results_for_evr_and_pm)
             print('row {:3} -> (enterprise_value_to_revenue_limit {:3}) | col {:3} -> (profit_margin_limit {:3}%): num_results_for_evr_and_pm = {}'.format(enterprise_value_to_revenue_limit-min_evr, enterprise_value_to_revenue_limit, profit_margin_limit-pm_min, profit_margin_limit, num_results_for_evr_and_pm))
-    results_filename_ssss    = 'results_ssss_evr{}-{}_pm{}-{}.csv'.format(min_evr,max_evr,pm_min,pm_max)
-    results_filename_sssss   = 'results_sssss_evr{}-{}_pm{}-{}.csv'.format(min_evr,max_evr,pm_min,pm_max)
-    np.savetxt(csv_db_path+'/'+results_filename_ssss,  research_rows_ssss.astype(int), fmt='%d', delimiter=',')
-    np.savetxt(csv_db_path+'/'+results_filename_sssss, research_rows_sssss.astype(int), fmt='%d', delimiter=',')
+    results_filename    = 'results_evr{}-{}_pm{}-{}.csv'.format(min_evr,max_evr,pm_min,pm_max)
+    np.savetxt(csv_db_path+'/'+results_filename,  research_rows_ssss.astype(int), fmt='%d', delimiter=',')
     title_row = list(range(pm_min,pm_max+1))
     title_row.insert(0, 'evr / pm')
-    evr_rows_pm_cols_filenames_list = [csv_db_path+'/'+results_filename_ssss, csv_db_path+'/'+results_filename_sssss]
+    evr_rows_pm_cols_filenames_list = [csv_db_path+'/'+results_filename]
     # Read Results, and add row and col axis:
     for filename in evr_rows_pm_cols_filenames_list:
         evr_rows_pm_cols = [title_row]
@@ -86,13 +84,13 @@ def research_db(min_evr, max_evr, pm_min, pm_max, csv_db_path, read_united_state
     sorted_appearance_counter_dict_sssss        = {k: v for k, v in sorted(appearance_counter_dict_sssss.items(), key=lambda item: item[1], reverse=True)}
     result_sorted_appearance_counter_dict_sssss = {k: v for k, v in sorted_appearance_counter_dict_sssss.items() if v > 0.0}
 
-    reccomendation_list_filename_ssss = csv_db_path+'/reccomendation_ssss_'+results_filename_ssss.replace('results_','')
+    reccomendation_list_filename_ssss = csv_db_path+'/reccomendation_ssss_'+results_filename.replace('results_','')
     with open(reccomendation_list_filename_ssss, 'w') as f:
         f.write("Ticker,Name,Sector,ssss_value,appearance_counter\n")
         for key in result_sorted_appearance_counter_dict_ssss.keys():
             f.write("%s,%s,%s,%s,%s\n"%(key[0],str(key[1]).replace(',',' '),key[2],key[3],result_sorted_appearance_counter_dict_ssss[key]))
 
-    reccomendation_list_filename_sssss = csv_db_path+'/reccomendation_sssss_'+results_filename_sssss.replace('results_','')
+    reccomendation_list_filename_sssss = csv_db_path+'/reccomendation_sssss_'+results_filename.replace('results_','')
     with open(reccomendation_list_filename_sssss, 'w') as f:
         f.write("Ticker,Name,Sector,sssss_value,appearance_counter\n")
         for key in result_sorted_appearance_counter_dict_sssss.keys():
@@ -100,11 +98,11 @@ def research_db(min_evr, max_evr, pm_min, pm_max, csv_db_path, read_united_state
 
 # TASE:
 # =====
-# research_db(min_evr=1, max_evr=25, pm_min=5,  pm_max=45, csv_db_path='Results/20210102-230029_Tase_FavorTechBy3_MCap_pm0.0567_evr15.0_BuildDb_nResults456',   read_united_states_input_symbols=0, tase_mode=1, generate_result_folders=0, appearance_counter_min=20, appearance_counter_max=40)
+# research_db(min_evr=1, max_evr=25, pm_min=5,  pm_max=45, csv_db_path='Results/20210117-021721_Tase_FavorTechBy3_MCap_pm0.0567_evr15.0_BuildDb_nResults438',   read_united_states_input_symbols=0, tase_mode=1, generate_result_folders=0, appearance_counter_min=5, appearance_counter_max=55)
 
 # NASDAQ100+S&P500+RUSSEL1000:
 # ============================
-research_db(min_evr=1, max_evr=45, pm_min=5, pm_max=50, csv_db_path='Results/20210116-220250_FavorTechBy3_MCap_pm0.17_evr17.5_BuildDb_nResults1083',         read_united_states_input_symbols=0, tase_mode=0, generate_result_folders=0, appearance_counter_min=5, appearance_counter_max=55)
+# research_db(min_evr=1, max_evr=45, pm_min=5, pm_max=50, csv_db_path='Results/20210116-220250_FavorTechBy3_MCap_pm0.17_evr17.5_BuildDb_nResults1083',         read_united_states_input_symbols=0, tase_mode=0, generate_result_folders=0, appearance_counter_min=5, appearance_counter_max=55)
 
 # Generate:
 # research_db(min_evr=6, max_evr=6,  pm_min=19, pm_max=19, csv_db_path='Results/20210116-220250_FavorTechBy3_MCap_pm0.17_evr17.5_BuildDb_nResults1083',         read_united_states_input_symbols=0, tase_mode=0, generate_result_folders=1, appearance_counter_min=20, appearance_counter_max=40)
