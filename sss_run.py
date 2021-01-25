e1c3c9dc427c3e0ac0fa111191bda3e715653b87
Asaf <46109899+asafravid@users.noreply.github.com>
2021-01-25 23:54:26 +0200

    V106
    Important bug fixes nd updates

--- sss_run.py
+++ sss_run.py
@@ -1,5 +1,5 @@
 #########################################################
-# Version 105 - Author: Asaf Ravid <asaf.rvd@gmail.com> #
+# Version 106 - Author: Asaf Ravid <asaf.rvd@gmail.com> #
 #########################################################
 
 
@@ -14,19 +14,19 @@ EV_MILLIONS_STEP = 100
 
 
 # Reuse:
-# sss.sss_run(sectors_list=[], build_csv_db_only=0, build_csv_db=0, csv_db_path='None', read_united_states_input_symbols=0, tase_mode=0, num_threads=1,  market_cap_included=1, use_investpy=0, research_mode=0, profit_margin_limit=0.17, ev_to_cfo_ratio_limit = 100.0, min_enterprise_value_millions_usd=100, best_n_select=50, enterprise_value_to_revenue_limit=15, favor_technology_sector_evr=4, generate_result_folders=1)
+# sss.sss_run(sectors_list=[], build_csv_db_only=0, build_csv_db=0, csv_db_path='None', read_united_states_input_symbols=0, tase_mode=0, num_threads=1,  market_cap_included=1, use_investpy=0, research_mode=0, profit_margin_limit=0.17, ev_to_cfo_ratio_limit = 100.0, min_enterprise_value_millions_usd=100, best_n_select=50, enterprise_value_to_revenue_limit=15, favor_technology_sector=4, generate_result_folders=1)
 
 # Run Build DB Only: Nasdaq100+S&P500+Russel1000
 # ==============================================
-# sss.sss_run(sectors_list=[], build_csv_db_only=1, build_csv_db=1, csv_db_path='None', read_united_states_input_symbols=0, tase_mode=0, num_threads=20, market_cap_included=1, use_investpy=0, research_mode=0, profit_margin_limit=0.12, ev_to_cfo_ratio_limit = 100.0, min_enterprise_value_millions_usd=100, best_n_select=3, enterprise_value_to_revenue_limit=20, favor_technology_sector_evr=4, generate_result_folders=1)
+# sss.sss_run(sectors_list=[], build_csv_db_only=1, build_csv_db=1, csv_db_path='None', read_united_states_input_symbols=0, tase_mode=0, num_threads=20, market_cap_included=1, use_investpy=0, research_mode=0, profit_margin_limit=0.12, ev_to_cfo_ratio_limit = 100.0, min_enterprise_value_millions_usd=100, best_n_select=3, enterprise_value_to_revenue_limit=20, favor_technology_sector=4, generate_result_folders=1)
 
 # Run Build DB Only: All/Others
 # =============================
-# sss.sss_run(sectors_list=[], build_csv_db_only=1, build_csv_db=1, csv_db_path='None', read_united_states_input_symbols=1, tase_mode=0, num_threads=20, market_cap_included=1, use_investpy=0, research_mode=0, profit_margin_limit=0.10, ev_to_cfo_ratio_limit = 100.0, min_enterprise_value_millions_usd=100, best_n_select=3, enterprise_value_to_revenue_limit=20, favor_technology_sector_evr=4, generate_result_folders=1)
+# sss.sss_run(sectors_list=[], build_csv_db_only=1, build_csv_db=1, csv_db_path='None', read_united_states_input_symbols=1, tase_mode=0, num_threads=20, market_cap_included=1, use_investpy=0, research_mode=0, profit_margin_limit=0.10, ev_to_cfo_ratio_limit = 100.0, min_enterprise_value_millions_usd=100, best_n_select=3, enterprise_value_to_revenue_limit=20, favor_technology_sector=4, generate_result_folders=1)
 
 # Run Build DB Only: TASE
 # =============================
-# sss.sss_run(sectors_list=[], build_csv_db_only=1, build_csv_db=1, csv_db_path='None', read_united_states_input_symbols=0, tase_mode=1, num_threads=20, market_cap_included=1, use_investpy=0, research_mode=0, profit_margin_limit=0.10, ev_to_cfo_ratio_limit = 100.0, min_enterprise_value_millions_usd=10, best_n_select=3, enterprise_value_to_revenue_limit=25, favor_technology_sector_evr=4, generate_result_folders=1)
+# sss.sss_run(sectors_list=[], build_csv_db_only=1, build_csv_db=1, csv_db_path='None', read_united_states_input_symbols=0, tase_mode=1, num_threads=20, market_cap_included=1, use_investpy=0, research_mode=0, profit_margin_limit=0.10, ev_to_cfo_ratio_limit = 100.0, min_enterprise_value_millions_usd=10, best_n_select=3, enterprise_value_to_revenue_limit=25, favor_technology_sector=4, generate_result_folders=1)
 
 
 # Research Mode:
@@ -46,7 +46,7 @@ def prepare_appearance_counters_dictionaries(csv_db_path, appearance_counter_dic
                 appearance_counter_dict_sssss[(row[0],row[1],row[2],float(row[5]))] = 0.0  # Symbol, Short Name, Sector, SSSSS Value
 
 
-def research_db(evr_range, pm_range, ev_millions_range, csv_db_path, read_united_states_input_symbols, tase_mode, generate_result_folders, appearance_counter_min, appearance_counter_max, favor_technology_sector_evr):
+def research_db(evr_range, pm_range, ev_millions_range, csv_db_path, read_united_states_input_symbols, tase_mode, generate_result_folders, appearance_counter_min, appearance_counter_max, favor_technology_sector):
     appearance_counter_dict_ssss  = {}
     appearance_counter_dict_sssss = {}
     prepare_appearance_counters_dictionaries(csv_db_path, appearance_counter_dict_ssss, appearance_counter_dict_sssss)
@@ -55,7 +55,7 @@ def research_db(evr_range, pm_range, ev_millions_range, csv_db_path, read_united
     for enterprise_value_to_revenue_limit in range(evr_range[0],evr_range[1]+1):
         for profit_margin_limit in range(pm_range[1],pm_range[0]-1,-1):
             min_enterprise_value_millions_usd = ev_millions_range  # ev_millions_range[0]+EV_MILLIONS_STEP*(ev_millions_limit_index-1)
-            num_results_for_evr_and_pm = sss.sss_run(sectors_list=[], build_csv_db_only=0, build_csv_db=0, csv_db_path=csv_db_path, read_united_states_input_symbols=read_united_states_input_symbols, tase_mode=tase_mode, num_threads=1, market_cap_included=1, use_investpy=0, research_mode=1, profit_margin_limit=float(profit_margin_limit)/100.0, min_enterprise_value_millions_usd=min_enterprise_value_millions_usd, ev_to_cfo_ratio_limit = 100.0, best_n_select=3, enterprise_value_to_revenue_limit=enterprise_value_to_revenue_limit, favor_technology_sector_evr=favor_technology_sector_evr, generate_result_folders=generate_result_folders, appearance_counter_dict_ssss=appearance_counter_dict_ssss, appearance_counter_dict_sssss=appearance_counter_dict_sssss, appearance_counter_min=appearance_counter_min, appearance_counter_max=appearance_counter_max)
+            num_results_for_evr_and_pm = sss.sss_run(sectors_list=[], build_csv_db_only=0, build_csv_db=0, csv_db_path=csv_db_path, read_united_states_input_symbols=read_united_states_input_symbols, tase_mode=tase_mode, num_threads=1, market_cap_included=1, use_investpy=0, research_mode=1, profit_margin_limit=float(profit_margin_limit)/100.0, min_enterprise_value_millions_usd=min_enterprise_value_millions_usd, ev_to_cfo_ratio_limit = 100.0, best_n_select=3, enterprise_value_to_revenue_limit=enterprise_value_to_revenue_limit, favor_technology_sector=favor_technology_sector, generate_result_folders=generate_result_folders, appearance_counter_dict_ssss=appearance_counter_dict_ssss, appearance_counter_dict_sssss=appearance_counter_dict_sssss, appearance_counter_min=appearance_counter_min, appearance_counter_max=appearance_counter_max)
             if num_results_for_evr_and_pm > appearance_counter_max: break  # already appearance_counter_max results. With lower profit margin limit there will always be more results -> saves running time
             research_rows_ssss[ enterprise_value_to_revenue_limit-evr_range[0]][profit_margin_limit-pm_range[0]] = int(num_results_for_evr_and_pm)
             research_rows_sssss[enterprise_value_to_revenue_limit-evr_range[0]][profit_margin_limit-pm_range[0]] = int(num_results_for_evr_and_pm)
@@ -102,29 +102,29 @@ def research_db(evr_range, pm_range, ev_millions_range, csv_db_path, read_united
 
 # TASE:
 # =====
-# old_run = 'Results/20210120-235652_Tase_FavorTechBy3_MCap_pm0.0567_evr15.0_BuildDb_nResults439'
-# new_run = 'Results/20210125-013126_TaseFTB3_MCap_pm0.0567_evr15.0_BuildDb_nResults454'
-# research_db(evr_range=[1,25], pm_range=[5,45], ev_millions_range=10,   csv_db_path=new_run,   read_united_states_input_symbols=0, tase_mode=1, generate_result_folders=0, appearance_counter_min=5, appearance_counter_max=65)
-# sss_diff.run(newer_path=new_run, older_path=old_run, db_exists_in_both_folders=1, diff_only_recommendation=1, ticker_index=0, name_index=1, movement_threshold=3, newer_rec_ranges=[10,1,25,5,45], older_rec_ranges=[500,1,25,5,45], rec_length=60)
+# old_run = 'Results/20210125-013126_TaseFTB3_MCap_pm0.0567_evr15.0_BuildDb_nResults454'
+# new_run = 'Results/20210125-153913_Tase_FTB4_MCap_pm0.0567_evr15.0_BuildDb_nResults454'
+# research_db(evr_range=[1,45], pm_range=[5,45], ev_millions_range=10,   csv_db_path=new_run,   read_united_states_input_symbols=0, tase_mode=1, generate_result_folders=0, appearance_counter_min=5, appearance_counter_max=75, favor_technology_sector=4)
+# sss_diff.run(newer_path=new_run, older_path=old_run, db_exists_in_both_folders=1, diff_only_recommendation=1, ticker_index=0, name_index=1, movement_threshold=3, newer_rec_ranges=[10,1,45,5,45], older_rec_ranges=[10,1,25,5,45], rec_length=75)
 
 # NASDAQ100+S&P500+RUSSEL1000:
 # ============================
-old_run = 'Results/20210124-010259FTB3_MCap_pm0.17_evr17.5_BuildDb_nResults1094'
-new_run = 'Results/20210125-030805_FTB4_MCap_pm0.17_evr17.5_BuildDb_nResults1094'
-# research_db(evr_range=[1,45], pm_range=[5,50], ev_millions_range=100,  csv_db_path=new_run,   read_united_states_input_symbols=0, tase_mode=0, generate_result_folders=0, appearance_counter_min=5, appearance_counter_max=65, favor_technology_sector_evr=4)
-sss_diff.run(newer_path=new_run, older_path=old_run, db_exists_in_both_folders=1, diff_only_recommendation=1, ticker_index=0, name_index=1, movement_threshold=3, newer_rec_ranges=[100,1,45,5,50], older_rec_ranges=[100,1,45,5,50], rec_length=60)
+old_run = 'Results/20210125-030805_FTB4_MCap_pm0.17_evr17.5_BuildDb_nResults1094'
+new_run = 'Results/20210125-230218_FTB4_MCap_pm0.17_evr17.5_BuildDb_nResults1126'
+# research_db(evr_range=[1,50], pm_range=[5,55], ev_millions_range=100,  csv_db_path=new_run,   read_united_states_input_symbols=0, tase_mode=0, generate_result_folders=0, appearance_counter_min=5, appearance_counter_max=70, favor_technology_sector=4)
+# sss_diff.run(newer_path=new_run, older_path=old_run, db_exists_in_both_folders=1, diff_only_recommendation=1, ticker_index=0, name_index=1, movement_threshold=3, newer_rec_ranges=[100,1,50,5,55], older_rec_ranges=[100,1,45,5,50], rec_length=60)
 
 # Generate:
-# research_db(evr_range=[7,7],  pm_range=[21,21], ev_millions_range=100, csv_db_path=new_run,   read_united_states_input_symbols=0, tase_mode=0, generate_result_folders=1, appearance_counter_min=20, appearance_counter_max=40)
+research_db(evr_range=[7,7],  pm_range=[22,22], ev_millions_range=100, csv_db_path=new_run,   read_united_states_input_symbols=0, tase_mode=0, generate_result_folders=1, appearance_counter_min=15, appearance_counter_max=45, favor_technology_sector=4)
 
 # ALL:
 # ====
 # old_run = 'Results/20210117-150719_FavorTechBy3_All_MCap_pm0.24_evr15.0_BuildDb_nResults6501'
 # new_run = 'Results/20210121-085550_FavorTechBy3_All_MCap_pm0.24_evr15.0_BuildDb_nResults6552'
-# research_db(evr_range=[1,55], pm_range=[5,45], ev_millions_range=100, csv_db_path=new_run,  read_united_states_input_symbols=1, tase_mode=0, generate_result_folders=0, appearance_counter_min=5, appearance_counter_max=55)
+# research_db(evr_range=[1,55], pm_range=[5,45], ev_millions_range=100, csv_db_path=new_run,  read_united_states_input_symbols=1, tase_mode=0, generate_result_folders=0, appearance_counter_min=5, appearance_counter_max=55, favor_technology_sector=4)
 # sss_diff.run(newer_path=new_run, older_path=old_run, db_exists_in_both_folders=1, diff_only_recommendation=1, ticker_index=0, name_index=1, movement_threshold=3, newer_rec_ranges=[100, 1, 55, 5, 45], older_rec_ranges=[100, 1, 55, 5, 45], rec_length=60)
 
 # Generate ALL:
-# research_db(evr_range=[7,7], pm_range=[32,32], ev_millions_range=100, csv_db_path=new_run,  read_united_states_input_symbols=1, tase_mode=0, generate_result_folders=1, appearance_counter_min=20, appearance_counter_max=40)
+# research_db(evr_range=[7,7], pm_range=[32,32], ev_millions_range=100, csv_db_path=new_run,  read_united_states_input_symbols=1, tase_mode=0, generate_result_folders=1, appearance_counter_min=20, appearance_counter_max=40, favor_technology_sector=4)
 
 
