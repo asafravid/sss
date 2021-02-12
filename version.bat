97f52660847c9e0139c168ddf8eed324dca0090e
Asaf <46109899+asafravid@users.noreply.github.com>
2021-02-12 12:01:57 +0200

    PDF Generator source version as well

--- version.bat
+++ version.bat
@@ -2,3 +2,5 @@ copy sss.py           sss_%1.py
 copy sss_run.py       sss_run_%1.py
 copy sss_diff.py      sss_diff_%1.py
 copy sss_filenames.py sss_filenames_%1.py
+copy sss_filenames.py sss_filenames_%1.py
+copy pdf_generator.py pdf_generator_%1.py 
