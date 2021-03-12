bb708c3a739d8222731f64a2d8a459e85b8d5ebd
Asaf <46109899+asafravid@users.noreply.github.com>
2021-03-12 22:06:08 +0200

    V275
    - Add quarterly earnings and revenue to database
    - Avoid 5 letter stocks with Y at the end (configurable) - as those are less easy to buy (mostly OTC)

--- version.bat
+++ version.bat
@@ -1,11 +1,12 @@
 REM #########################################################
-REM # Version 200- Author: Asaf Ravid <asaf.rvd@gmail.com> #
+REM # Version 275 - Author: Asaf Ravid <asaf.rvd@gmail.com> #
 REM #########################################################
 
-copy sss.py           Backup\sss_%1.py
-copy sss_run.py       Backup\sss_run_%1.py
-copy sss_diff.py      Backup\sss_diff_%1.py
-copy sss_filenames.py Backup\sss_filenames_%1.py
-copy sss_filenames.py Backup\sss_filenames_%1.py
-copy pdf_generator.py Backup\pdf_generator_%1.py 
-copy version.bat Backup\version_%1.bat 
+mkdir Backup\%1
+copy sss.py           Backup\%1\sss.py
+copy sss_run.py       Backup\%1\sss_run.py
+copy sss_diff.py      Backup\%1\sss_diff.py
+copy sss_filenames.py Backup\%1\sss_filenames.py
+copy sss_filenames.py Backup\%1\sss_filenames.py
+copy pdf_generator.py Backup\%1\pdf_generator.py 
+copy version.bat Backup\%1\version.bat 
