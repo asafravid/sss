e1ec2a4cf06d46f8c03e51711414e6f71455a0cb
Asaf <46109899+asafravid@users.noreply.github.com>
2021-04-22 01:46:18 +0300

    V 0.0.505
    Scan full folders within a folder

--- sss_results_performance.py
+++ sss_results_performance.py
@@ -1,6 +1,6 @@
 #############################################################################
 #
-# Version 0.0.500 - Author: Asaf Ravid <asaf.rvd@gmail.com>
+# Version 0.0.505 - Author: Asaf Ravid <asaf.rvd@gmail.com>
 #
 #    Stock Screener and Scanner - based on yfinance and investpy
 #    Copyright (C) 2021 Asaf Ravid
@@ -20,21 +20,27 @@
 #
 #############################################################################
 
+from glob import glob
 import csv
 import yfinance as yf
 import datetime
 import numpy
+import math
 
 import sss
 
-TASE_MODE          = 0
-RESULTS_INPUT_PATH = "Results/Nsr/20201115-043117_FAVOUR_TECH_BY3_MARKETCAP_FORWARDEPS_PMARGIN0.17_EVR17.5_BUILD_DB_ONLY_NUM_RESULTS_1115"
+END_DATE_STR = '20210419'
 RESULTS_LEN        = 28
+TASE_MODE          = 0
+RESULTS_INPUT_FOLDER = "Results/Nsr"
+RESULTS_INPUT_PATH = "Results/Nsr/20201115-043117_FAVOUR_TECH_BY3_MARKETCAP_FORWARDEPS_PMARGIN0.17_EVR17.5_BUILD_DB_ONLY_NUM_RESULTS_1115"
+
+results_input_paths = glob(RESULTS_INPUT_FOLDER+'/*/')
 
 
 # Read Results Input:
 def read_engine_results(path, max_results, sss_value_name):
-    results_list        = []
+    engine_results_list = []
     effective_row_index = 0
     sss_value_index     = -1
 
@@ -50,41 +56,42 @@ def read_engine_results(path, max_results, sss_value_name):
                     continue
                 else:
                     if sss_value_index >= 0 and float(row[sss_value_index]) > 0.0:
-                        results_list.append(row[0])
+                        engine_results_list.append(row[0])
                         effective_row_index += 1
                         if effective_row_index >= max_results:
                             break
     except Exception as e:
-        print('Exception {} in [read_engine_results]: path {}, max_results {}, sss_value_name {}'.format(e, path, max_results, sss_value_name))
-    return results_list
+        # print('Exception {} in [read_engine_results]: path {}, max_results {}, sss_value_name {}'.format(e, path, max_results, sss_value_name))
+        pass
+    return engine_results_list
 
-results_date = RESULTS_INPUT_PATH.replace("Results","").replace("Nsr","").replace("All","").replace("Tase","").replace("/","")[:8]
-# today_str = time.strftime("%Y%m%d")
-end_date_str = '20210419'
-#                             year                    month                   day
-start = datetime.datetime(int(results_date[0:4]), int(results_date[4:6]), int(results_date[6:8]))
-end   = datetime.datetime(int(end_date_str[0:4]), int(end_date_str[4:6]), int(end_date_str[6:8]))
-# data_start = yf.download(results_list, start=start, end=start, threads=False)
-# data_end   = yf.download(results_list, start=end,   end=end  , threads=False)
 
-sss_values_list = ['sss', 'ssss', 'sssss']
-results_lists    = []
-gains_lists      = []
-performance_list = []
-for sss_value_name in sss_values_list:
-    results_list = read_engine_results(RESULTS_INPUT_PATH+'/'+sss_value_name+'_engine.csv', RESULTS_LEN, sss_value_name+'_value')
-    results_lists.append(results_list)
+for results_input_path in results_input_paths:
+    results_input_path = results_input_path.replace("\\",'/')[:-1]
+    results_date       = results_input_path.replace("Results","").replace("Nsr","").replace("All","").replace("Tase","").replace("/","")[:8]
+    #                             year                    month                   day
+    start = datetime.datetime(int(results_date[0:4]), int(results_date[4:6]), int(results_date[6:8]))
+    end   = datetime.datetime(int(END_DATE_STR[0:4]), int(END_DATE_STR[4:6]), int(END_DATE_STR[6:8]))
 
-    gains_list  = []
-    performance = None
-    if len(results_list):
-        data_start_end = yf.download(results_list, start=start, end=end, threads=False)
-        for symbol in results_list:
-            start_date_value = data_start_end.Close[symbol][0]
-            end_date_value   = data_start_end.Close[symbol][-1]
-            gains_list.append(round(end_date_value/start_date_value-1.0, sss.NUM_ROUND_DECIMALS))
-        performance = round(100*numpy.mean(gains_list), sss.NUM_ROUND_DECIMALS)
-    gains_lists.append(gains_list)
-    performance_list.append(performance)
+    sss_values_list = ['sss', 'ssss', 'sssss']
+    results_lists    = []
+    gains_lists      = []
+    performance_list = []
+    for sss_value_name in sss_values_list:
+        results_list = read_engine_results(results_input_path+'/'+sss_value_name+'_engine.csv', RESULTS_LEN, sss_value_name+'_value')
+        results_lists.append(results_list)
 
-print('Performance % between {} and {} of {} is {}'.format(start,end, sss_values_list, performance_list))
+        gains_list  = []
+        performance = None
+        if len(results_list):
+            data_start_end = yf.download(results_list, start=start, end=end, threads=False)
+            for symbol in results_list:
+                start_date_value = data_start_end.Close[symbol][0]
+                end_date_value   = data_start_end.Close[symbol][-1]
+                if not math.isnan(start_date_value) and not math.isnan(end_date_value):
+                    gains_list.append(round(end_date_value/start_date_value-1.0, sss.NUM_ROUND_DECIMALS))
+            performance = round(100*numpy.mean(gains_list), sss.NUM_ROUND_DECIMALS)
+        gains_lists.append(gains_list)
+        performance_list.append(performance)
+
+    print('Performance % between {} and {} of {} is {}'.format(start,end, sss_values_list, performance_list))
