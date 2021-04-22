3b14b38572cca63abc4127029957cdc9280fd51c
Asaf <46109899+asafravid@users.noreply.github.com>
2021-04-22 22:55:54 +0300

    V 0.0.506
    Existence in DB ratios researce

--- sss_results_performance.py
+++ sss_results_performance.py
@@ -1,6 +1,6 @@
 #############################################################################
 #
-# Version 0.0.505 - Author: Asaf Ravid <asaf.rvd@gmail.com>
+# Version 0.0.506 - Author: Asaf Ravid <asaf.rvd@gmail.com>
 #
 #    Stock Screener and Scanner - based on yfinance and investpy
 #    Copyright (C) 2021 Asaf Ravid
@@ -66,32 +66,51 @@ def read_engine_results(path, max_results, sss_value_name):
     return engine_results_list
 
 
+symbol_close_values_db = {}
+
+
 for results_input_path in results_input_paths:
     results_input_path = results_input_path.replace("\\",'/')[:-1]
+    print('\n Analyzing {}:'.format(results_input_path))
     results_date       = results_input_path.replace("Results","").replace("Nsr","").replace("All","").replace("Tase","").replace("/","")[:8]
     #                             year                    month                   day
     start = datetime.datetime(int(results_date[0:4]), int(results_date[4:6]), int(results_date[6:8]))
     end   = datetime.datetime(int(END_DATE_STR[0:4]), int(END_DATE_STR[4:6]), int(END_DATE_STR[6:8]))
 
-    sss_values_list = ['sss', 'ssss', 'sssss']
-    results_lists    = []
-    gains_lists      = []
-    performance_list = []
+    sss_values_list             = ['sss', 'ssss', 'sssss']
+    results_lists               = []
+    gains_lists                 = []
+    performance_list            = []
+    existence_in_db_ratios_list = []
     for sss_value_name in sss_values_list:
         results_list = read_engine_results(results_input_path+'/'+sss_value_name+'_engine.csv', RESULTS_LEN, sss_value_name+'_value')
         results_lists.append(results_list)
 
-        gains_list  = []
-        performance = None
+        existing_symbols_list = []
+        new_symbols_list      = []
+        gains_list            = []
+        performance           = None
+        existence_in_db_ratio = None
         if len(results_list):
+            # Assuming folders are traversed alpphabetically, they will be scanned from oldest to newest, so dates may already exist:
+            for symbol in results_list:
+                if symbol in symbol_close_values_db:
+                    existing_symbols_list.append(symbol)
+                else:
+                    new_symbols_list.append(symbol)
+
             data_start_end = yf.download(results_list, start=start, end=end, threads=False)
             for symbol in results_list:
+                if symbol not in symbol_close_values_db:
+                    symbol_close_values_db[symbol] = data_start_end.Close[symbol]
                 start_date_value = data_start_end.Close[symbol][0]
                 end_date_value   = data_start_end.Close[symbol][-1]
                 if not math.isnan(start_date_value) and not math.isnan(end_date_value):
                     gains_list.append(round(end_date_value/start_date_value-1.0, sss.NUM_ROUND_DECIMALS))
-            performance = round(100*numpy.mean(gains_list), sss.NUM_ROUND_DECIMALS)
+            performance           = round(100*numpy.mean(gains_list), sss.NUM_ROUND_DECIMALS)
+            existence_in_db_ratio = round(float(len(existing_symbols_list))/float(len(results_list)),sss.NUM_ROUND_DECIMALS)
         gains_lists.append(gains_list)
         performance_list.append(performance)
+        existence_in_db_ratios_list.append(existence_in_db_ratio)
 
-    print('Performance % between {} and {} of {} is {}'.format(start,end, sss_values_list, performance_list))
+    print('     Performance % between {} and {} of {} is {}. DB existence ({})'.format(start,end, sss_values_list, performance_list, existence_in_db_ratios_list))
