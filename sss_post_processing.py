2f37000d6bbf5a5b1c477b5cbc93548fb1af4056
Asaf <46109899+asafravid@users.noreply.github.com>
2021-07-10 23:35:03 +0300

    V 0.1.105
    Sort by sss_value_normalized + fix normalized columns positions

--- sss_post_processing.py
+++ sss_post_processing.py
@@ -1,6 +1,6 @@
 #############################################################################
 #
-# Version 0.1.103 - Author: Asaf Ravid <asaf.rvd@gmail.com>
+# Version 0.1.105 - Author: Asaf Ravid <asaf.rvd@gmail.com>
 #
 #    Stock Screener and Scanner - based on yfinance
 #    Copyright (C) 2021 Asaf Ravid
@@ -27,6 +27,7 @@ import sss
 import sss_filenames
 
 SSS_VALUE_NORMALIZED_COLUMN_NAME = "sss_value_normalized"
+SSS_VALUE_COLUMN_NAME            = "sss_value"
 
 
 def process_engine_csv(path) -> object:
@@ -35,14 +36,19 @@ def process_engine_csv(path) -> object:
     max_values = data.max()
     [numerator_parameters_list, denominator_parameters_list] = sss.get_used_parameters_names_in_core_equation()
     for parameter in numerator_parameters_list:
-        data[parameter+"_normalized"] = data[parameter] / max_values[parameter]
+        new_column       = data[parameter] / max_values[parameter]
+        new_column_index = data.columns.get_loc(parameter)
+        data.insert(new_column_index+1, parameter+"_normalized",new_column)
         if SSS_VALUE_NORMALIZED_COLUMN_NAME in data:
             data[SSS_VALUE_NORMALIZED_COLUMN_NAME] = data[SSS_VALUE_NORMALIZED_COLUMN_NAME] + data[parameter+"_normalized"]
         else:
-            data[SSS_VALUE_NORMALIZED_COLUMN_NAME] =                                          data[parameter+"_normalized"]
+            new_column_index = data.columns.get_loc(SSS_VALUE_COLUMN_NAME)
+            data.insert(new_column_index + 1, SSS_VALUE_NORMALIZED_COLUMN_NAME, new_column)
     for parameter in denominator_parameters_list:
-        data[parameter+"_normalized"] = data[parameter] / max_values[parameter]
+        new_column       = data[parameter] / max_values[parameter]
+        new_column_index = data.columns.get_loc(parameter)
+        data.insert(new_column_index+1, parameter+"_normalized",new_column)
         data[SSS_VALUE_NORMALIZED_COLUMN_NAME]     = data[SSS_VALUE_NORMALIZED_COLUMN_NAME] - data[parameter+"_normalized"]
 
-    data.sort_values(by=[SSS_VALUE_NORMALIZED_COLUMN_NAME])
-    data.to_csv(filename_path+"_normalized.csv")
+    sorted_Data = data.sort_values(by=[SSS_VALUE_NORMALIZED_COLUMN_NAME])
+    sorted_Data.to_csv(filename_path+"_normalized.csv")
