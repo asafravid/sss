e0c6d21e2d142523b8ad90dd5a2c52e48901d0c7
asafravid <asaf.rvd@gmail.com>
2021-08-20 14:47:42 +0300

    - Fix stdout fixed column widths and more informative logs

--- sss_config.py
+++ sss_config.py
@@ -22,11 +22,11 @@
 
 
 run_custom_tase      = False   # Custom Portfolio
-run_custom           = False
+run_custom           = True
 run_tase             = False    # Tel Aviv Stock Exchange
 run_nsr              = False   # NASDAQ100+S&P500+RUSSEL1000
-run_all              = True   # All Nasdaq Stocks
-research_mode        = True    # Research Mode
+run_all              = False   # All Nasdaq Stocks
+research_mode        = False    # Research Mode
 aggregate_only       = False
 research_mode_max_ev = False   # @JustLearning's suggestion in Telegram: Multi-Dimensional Scan by Max EV Limit rather than Min EV Limit
 
@@ -39,15 +39,15 @@ automatic_results_folder_selection = False
 # Upon 1st ever run: reference must be set to None
 # After 1st ever Run: Recommended to use reference (filter and damper)
 # The research mode shall run on new_run as input (new_run >= reference_run) where > means newer
-reference_run_custom = None # 'Results/Custom/20210816-014004_Bdb_nRes289_Custom'
+reference_run_custom = 'Results/Custom/20210816-014004_Bdb_nRes289_Custom'
 reference_run_tase   = 'Results/Tase/20210813-001618_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes329'
-reference_run_nsr    = 'Results/Nsr/20210815-042855_Tchnlgy3.0_FnnclSrvcs0.5_Bdb_nRes881'
-reference_run_all    = 'Results/All/20210815-173943_Tchnlgy3.0_FnnclSrvcs0.5_A_Bdb_nRes3409'
+reference_run_nsr    = None # 'Results/Nsr/20210815-042855_Tchnlgy3.0_FnnclSrvcs0.5_Bdb_nRes881'
+reference_run_all    = None # 'Results/All/20210815-173943_Tchnlgy3.0_FnnclSrvcs0.5_A_Bdb_nRes3409'
 
 new_run_custom = 'Results/Custom/20210816-014004_Bdb_nRes289_Custom'
-new_run_tase   = 'Results/Tase/20210813-001618_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes329'
+new_run_tase   = 'Results/Tase/20210816-234500_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes329'
 new_run_nsr    = 'Results/Nsr/20210815-042855_Tchnlgy3.0_FnnclSrvcs0.5_Bdb_nRes881'
 new_run_all    = 'Results/All/20210817-144654_Tchnlgy3.0_FnnclSrvcs0.5_A_Bdb_nRes3414'
 
-custom_portfolio      = ['KMI']
+custom_portfolio      = ['DAC', 'AGO', 'WAL', 'AMAT', 'DFS', 'PNFP', 'LRCX', 'CACC', 'SLM', 'BPOP', 'MGM', 'OZK', 'KIM', 'KLAC', 'AFL', 'TROW', 'UI', 'FRC', 'BLK', 'SCHW', 'HIW', 'EXR', 'TROX', 'MRVL', 'FHN', 'SBNY', 'OMF', 'CPRX', 'MSFT', 'FCNCA', 'HZNP', 'UTHR', 'NUE', 'CSCO', 'TER', 'OLP', 'BIO', 'ORCL', 'NVO', 'SCI', 'MU', 'DLB', 'ABBV', 'GS', 'WTFC', 'EBAY', 'PM', 'BOKF', 'BXP', 'REGN', 'TGT', 'BDN', 'PBCT', 'COF', 'FB', 'GPP', 'TX', 'INVA', 'FDUS', 'GRMN', 'COOP', 'PB', 'ALL', 'MCO', 'QRVO', 'DGX', 'CGNX', 'ALGN', 'MS', 'ACN', 'BIIB', 'AVB', 'DRE', 'CE', 'TMO', 'AAPL', 'DG', 'FAST', 'AL', 'GTLS', 'ARE', 'ESGR', 'ACGL', 'LEN', 'LH', 'DHI', 'CDW', 'VICI', 'MDLZ', 'GTN', 'DISH', 'AKAM', 'ALLY', 'MMM', 'NGG', 'BAC', 'CDNS', 'COO', 'GNTX', 'GLPI', 'CEPU', 'ICE', 'FRO', 'ATH', 'BRK.B', 'CMCSA', 'PG', 'CUZ', 'ABEV', 'HOLX', 'MTH', 'PEG', 'WPC', 'TRNO', 'TEL', 'JEF', 'ETR', 'IMOS', 'UHS', 'UNH', 'WMT', 'TSCO', 'STT', 'NVR', 'CB', 'BLDR', 'APH', 'ZIM', 'WPM', 'SYF', 'USB', 'MAS', 'OHI', 'NOC', 'MT', 'PHM', 'VMW', 'VEDL', 'TRV', 'CCS', 'CNC', 'KNX', 'LEN.B', 'FTV', 'LITE', 'CRUS', 'STLD', 'KNOP', 'TSN', 'WFC', 'C', 'TXN', 'GD', 'GFI', 'TKC', 'NVEC', 'ATVI', 'TOL', 'SRE', 'THO', 'PNC', 'RGLD', 'MHO', 'JPM', 'STZ', 'CE', 'PBFX', 'VIV', 'DOX', 'HD', 'MX', 'PGR', 'AVGO', 'CIG', 'FCPT', 'SEB', 'AMCR', 'AMG', 'BTI', 'SNDR', 'ANTM', 'VZ', 'ES', 'VST', 'GHC', 'LMT', 'VRSN', 'CHKP', 'TRP', 'SSNC', 'GIB', 'TTEC', 'TLK', 'RKT', 'FDX', 'RE', 'WY', 'ZION', 'SCCO', 'MBT', 'AMGN', 'MARA', 'BMRN', 'SNX', 'WLK', 'GIS', 'VIACA', 'LDOS', 'ADM', 'QCOM', 'ELP', 'ARW', 'SWK', 'TRQ', 'SONY', 'MRK', 'TSM', 'ASX', 'RNR', 'ORAN', 'AZPN', 'MDC', 'FOXA', 'TM', 'BWXT', 'OPRA', 'RILY', 'SWKS', 'HTH', 'KMI', 'NUS', 'NEM', 'ONEW', 'BBY', 'PHI', 'CPB', 'MKSI', 'BMA', 'ZUMZ', 'LPX', 'MFC', 'WSM', 'RS', 'FOX', 'INTC', 'PCAR', 'HPQ', 'FLGT', 'GGB', 'VALE', 'FKWL', 'DD', 'RF', 'LYB', 'ZM', 'HUN', 'SMFG', 'CI', 'SKM', 'UWMC', 'LOGI', 'ENVA', 'TDS', 'CDK', 'RIO', 'KGC', 'JAZZ', 'TPC', 'SUPN', 'PRIM', 'MRCY', 'PKX', 'NBIX', 'ZTO', 'LOPE', 'ACTG', 'JD', 'NTES', 'SBSW', 'BTG', 'HMY', 'VRTX', 'SID', 'BIDU', 'BABA', 'LAKE', 'EXEL', 'LEJU', 'HAPP', 'FF', 'QDEL', 'GRVY', 'APT', 'YY', 'HMLP', 'HIMX', 'IRCP']
 custom_portfolio_tase = ['FORTY.TA']
