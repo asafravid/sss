eee0f9faff45a31db413b0a6545336b29d2d811c
asafravid <asaf.rvd@gmail.com>
2021-08-20 09:53:20 +0300

    - Update 20210815 NSR Results

--- sss_config.py
+++ sss_config.py
@@ -23,9 +23,9 @@
 
 run_custom_tase      = False   # Custom Portfolio
 run_custom           = False
-run_tase             = False    # Tel Aviv Stock Exchange
+run_tase             = True    # Tel Aviv Stock Exchange
 run_nsr              = False   # NASDAQ100+S&P500+RUSSEL1000
-run_all              = True   # All Nasdaq Stocks
+run_all              = False   # All Nasdaq Stocks
 research_mode        = True    # Research Mode
 aggregate_only       = False
 research_mode_max_ev = False   # @JustLearning's suggestion in Telegram: Multi-Dimensional Scan by Max EV Limit rather than Min EV Limit
@@ -40,7 +40,7 @@ automatic_results_folder_selection = False
 # After 1st ever Run: Recommended to use reference (filter and damper)
 # The research mode shall run on new_run as input (new_run >= reference_run) where > means newer
 reference_run_custom = None # 'Results/Custom/20210816-014004_Bdb_nRes289_Custom'
-reference_run_tase   = 'Results/Tase/20210813-001618_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes329'
+reference_run_tase   = 'Results/Tase/20210820-030153_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes324_'
 reference_run_nsr    = 'Results/Nsr/20210815-042855_Tchnlgy3.0_FnnclSrvcs0.5_Bdb_nRes881'
 reference_run_all    = 'Results/All/20210815-173943_Tchnlgy3.0_FnnclSrvcs0.5_A_Bdb_nRes3409'
 
