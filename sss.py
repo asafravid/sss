1e6a8637df65129613e29731a440cb07c25d8f26
asafravid <asaf.rvd@gmail.com>
2021-09-19 19:49:34 +0300

    - Profiling link

--- sss.py
+++ sss.py
@@ -1,6 +1,6 @@
 #############################################################################
 #
-# Version 0.2.45 - Author: Asaf Ravid <asaf.rvd@gmail.com>
+# Version 0.2.46 - Author: Asaf Ravid <asaf.rvd@gmail.com>
 #
 #    Stock Screener and Scanner - based on yfinance
 #    Copyright (C) 2021 Asaf Ravid
@@ -24,6 +24,7 @@
 # TODO: ASAFR:  0. Auto-Update Nasdaq (NSR) Indices as done with TASE
 #               0.1. Add All Swiss   Stocks to A dedicated scan: https://www.six-group.com/en/products-services/the-swiss-stock-exchange/market-data/shares/closing-prices.html, https://www.six-group.com/fqs/closing.csv?select=ShortName,ISIN,ValorSymbol,ValorNumber,ClosingPrice,DailyHighPrice,DailyLowPrice,LatestTradeDate,PreviousClosingPrice,OpeningPrice,OnMarketVolume,OffBookVolume,SwissAtMidVolume,TotalVolume,TradingBaseCurrency,YearlyHighDate,YearlyHighPrice,YearlyLowDate,YearlyLowPrice,FirstTradingDate,LastTradingDate,Exchange,SecTypeCode,GeographicalAreaCode,Tminus1Volume,VWAP60Price&where=ProductLine=BC&orderby=ShortName&page=1&pagesize=9999999
 #               0.2. Run a profiler on the research_mode to try and make it faster (and thus ready for more dimensions!)
+#               0.2.1. Native profiler: https://docs.python.org/3/library/profile.html
 #               1.0. https://en.wikipedia.org/wiki/Piotroski_F-score
 #               1.2. https://en.wikipedia.org/wiki/Magic_formula_investing
 #               1.3. https://www.oldschoolvalue.com/investing-strategy/backtest-graham-nnwc-ncav-screen/
