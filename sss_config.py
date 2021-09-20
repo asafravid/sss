1980d5d2f33f7c4035cae841d86e226553a92bcb
asafravid <asaf.rvd@gmail.com>
2021-09-20 10:31:54 +0300

    - Added Swiss stocks results 20210920 sanity check after optimizations

--- sss_config.py
+++ sss_config.py
@@ -29,8 +29,8 @@ run_custom_tase      = False   # Custom Portfolio
 run_custom           = False
 run_tase             = False    # Tel Aviv Stock Exchange
 run_nsr              = False   # NASDAQ100+S&P500+RUSSEL1000
-run_all              = True   # All Nasdaq Stocks
-run_six              = False    # All Swiss Stocks
+run_all              = False   # All Nasdaq Stocks
+run_six              = True    # All Swiss Stocks
 run_st               = False    # All (Stockholm) Swedish Stocks
 research_mode        = True    # Research Mode
 aggregate_only       = False
@@ -49,14 +49,14 @@ reference_run_custom = 'Results/Custom/20210909-154648_Bdb_nRes309_Custom'
 reference_run_tase   = 'Results/Tase/20210915-100103_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes333'
 reference_run_nsr    = 'Results/Nsr/20210912-133814_Tchnlgy3.0_FnnclSrvcs0.5_Bdb_nRes889'
 reference_run_all    = 'Results/All/20210912-092015_Tchnlgy3.0_FnnclSrvcs0.5_A_Bdb_nRes3409'
-reference_run_six    = 'Results/Six/20210910-085050_S_Bdb_nRes30'
+reference_run_six    = 'Results/Six/20210919-072727_S_Bdb_nRes30'
 reference_run_st     = 'Results/St/20210915-023602_St_Bdb_nRes130'
 
 new_run_custom = 'Results/Custom/20210917-201728_Bdb_nRes312_Custom'
 new_run_tase   = 'Results/Tase/20210915-100103_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes333'
 new_run_nsr    = 'Results/Nsr/20210912-133814_Tchnlgy3.0_FnnclSrvcs0.5_Bdb_nRes889'
 new_run_all    = 'Results/All/20210912-092015_Tchnlgy3.0_FnnclSrvcs0.5_A_Bdb_nRes3409'
-new_run_six    = 'Results/Six/20210919-072727_S_Bdb_nRes30'
+new_run_six    = 'Results/Six/20210920-101404_S_Bdb_nRes30'
 new_run_st     = 'Results/St/20210915-023602_St_Bdb_nRes130'
 
 # TODO: ASAFR: Why is normalized AGIO giving a Good low value?? this is deceiving! Check this
