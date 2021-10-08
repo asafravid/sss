00565f0b5c9b460f6c51d84ab0b7d09759f5b0a9
asafravid <asaf.rvd@gmail.com>
2021-10-08 15:33:51 +0300

    - Add 20211008 Results TASE

--- sss_config.py
+++ sss_config.py
@@ -34,7 +34,7 @@ run_nsr              = False   # NASDAQ100+S&P500+RUSSEL1000
 run_all              = False   # All Nasdaq Stocks
 run_six              = False    # All Swiss Stocks
 run_st               = False    # All (Stockholm) Swedish Stocks
-research_mode        = True    # Research Mode
+research_mode        = True     # Research Mode
 aggregate_only       = False
 research_mode_max_ev = False   # @JustLearning's suggestion in Telegram: Multi-Dimensional Scan by Max EV Limit rather than Min EV Limit
 
@@ -48,14 +48,14 @@ automatic_results_folder_selection = False
 # After 1st ever Run: Recommended to use reference (filter and damper)
 # The research mode shall run on new_run as input (new_run >= reference_run) where > means newer
 reference_run_custom = 'Results/Custom/20210909-154648_Bdb_nRes309_Custom'
-reference_run_tase   = 'Results/Tase/20211001-103934_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes335'
+reference_run_tase   = 'Results/Tase/20211008-093317_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes336'
 reference_run_nsr    = 'Results/Nsr/20210926-093332_Tchnlgy3.0_FnnclSrvcs0.75_Bdb_nRes898'
 reference_run_all    = 'Results/All/20210926-043839_Tchnlgy3.0_FnnclSrvcs0.75_A_Bdb_nRes3441'
 reference_run_six    = 'Results/Six/20210919-072727_S_Bdb_nRes30'
 reference_run_st     = 'Results/St/20210915-023602_St_Bdb_nRes130'
 
 new_run_custom = 'Results/Custom/20210917-201728_Bdb_nRes312_Custom'
-new_run_tase   = 'Results/Tase/20211001-103934_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes335'
+new_run_tase   = 'Results/Tase/20211008-093317_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes336'
 new_run_nsr    = 'Results/Nsr/20210926-093332_Tchnlgy3.0_FnnclSrvcs0.75_Bdb_nRes898'
 new_run_all    = 'Results/All/20210926-043839_Tchnlgy3.0_FnnclSrvcs0.75_A_Bdb_nRes3441'
 new_run_six    = 'Results/Six/20210920-174034_S_Bdb_nRes30'
