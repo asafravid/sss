64db723412951d72d0cf94c3cdcef1113088a91e
Asaf Ravid <aravid@marvell.com>
2021-11-10 00:59:41 +0200

    - Add a Custom Portfolio of False alarm (it seems) - debug, understand and fix

--- sss_config.py
+++ sss_config.py
@@ -1,6 +1,6 @@
 #############################################################################
 #
-# Version 0.2.56 - Author: Asaf Ravid <asaf.rvd@gmail.com>
+# Version 0.2.57 - Author: Asaf Ravid <asaf.rvd@gmail.com>
 #
 #    Stock Screener and Scanner - based on yfinance
 #    Copyright (C) 2021 Asaf Ravid
@@ -28,8 +28,8 @@ ALL_COUNTRY_SYMBOLS_SIX = 2  # Swiss Stock Exchange
 ALL_COUNTRY_SYMBOLS_ST  = 3  # Swedish (Stockholm) Stock Exchange
 
 run_custom_tase           = False   # Custom Portfolio
-run_custom                = False
-run_tase                  = True    # Tel Aviv Stock Exchange
+run_custom                = True
+run_tase                  = False    # Tel Aviv Stock Exchange
 run_nsr                   = False   # NASDAQ100+S&P500+RUSSEL1000
 run_all                   = False   # All Nasdaq Stocks
 run_six                   = False    # All Swiss Stocks
@@ -37,7 +37,7 @@ run_st                    = False    # All (Stockholm) Swedish Stocks
 research_mode             = False     # Research Mode
 aggregate_only            = False
 research_mode_max_ev      = False   # @JustLearning's suggestion in Telegram: Multi-Dimensional Scan by Max EV Limit rather than Min EV Limit
-use_reference_as_raw_data = False
+use_reference_as_raw_data = True
 
 # When automatic_results_folder_selection is False, the explicitly specified paths below are used for the
 # reference and new_run folder locations.
@@ -48,7 +48,7 @@ automatic_results_folder_selection = False
 # Upon 1st ever run: reference must be set to None
 # After 1st ever Run: Recommended to use reference (filter and damper)
 # The research mode shall run on new_run as input (new_run >= reference_run) where > means newer
-reference_run_custom = 'Results/Custom/20211108-002201_Bdb_nRes18_Custom'
+reference_run_custom = 'Results/Custom/20211109-235448_Bdb_nRes271_Custom'
 reference_run_tase   = 'Results/Tase/20211105-113630_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes291'
 reference_run_nsr    = 'Results/Nsr/20211106-170744_Tchnlgy3.0_FnnclSrvcs0.75_Bdb_nRes788'
 reference_run_all    = 'Results/All/20211107-151952_Tchnlgy3.0_FnnclSrvcs0.75_A_Bdb_nRes3004'
@@ -62,8 +62,8 @@ new_run_all    = 'Results/All/20211107-151952_Tchnlgy3.0_FnnclSrvcs0.75_A_Bdb_nR
 new_run_six    = 'Results/Six/20211012-141034_S_Bdb_nRes30'
 new_run_st     = 'Results/St/20210915-023602_St_Bdb_nRes130'
 
-# TODO: ASAFR: Why is normalized AGIO giving a Good low value?? this is deceiving! Check this
-custom_portfolio      = ['NVDA', 'BLK', 'DAC', 'X', 'TEVA', 'NICE', 'A', 'T', 'TSLA', 'AAPL', 'MSFT', 'BBBY', 'COST', 'ZM', 'CHKP', 'AMZN', 'SEDG', 'FB', 'NFLX', 'Y', 'C']
+# TODO: ASAFR: Why is debt_to_equity_effective_used    <= 0   a common failure to profitable stocks? investigate loss of data from yfinance... All of those below seem to be false alarms:
+custom_portfolio      = ['CNC', 'WMT', 'COLM', 'DD', 'DHI', 'FAST', 'FCPT', 'FTV', 'MHK', 'MRK', 'MRNA', 'MU', 'MX', 'NVEC', 'NVO', 'OHI', 'OLLI', 'OPRA', 'PGR', 'PM', 'PRIM', 'RGLD', 'RS', 'SK', 'SNX', 'SSNC', 'STZ', 'SYF', 'THO', 'TPC', 'UNH', 'USM', 'VIAC', 'WMT', 'ZUMZ']
 custom_portfolio_tase = ['APLP', 'ITMR']
 
 research_mode_probe_list = []  # ['MTDS']
