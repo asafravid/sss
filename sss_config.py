0156078eba182254324e5d531e0c7f59a26e8892
asafravid <asaf.rvd@gmail.com>
2021-11-12 10:01:59 +0200

    - Add 20211112 TASE Results

--- sss_config.py
+++ sss_config.py
@@ -29,9 +29,9 @@ ALL_COUNTRY_SYMBOLS_ST  = 3  # Swedish (Stockholm) Stock Exchange
 
 run_custom_tase           = False   # Custom Portfolio
 run_custom                = False
-run_tase                  = False    # Tel Aviv Stock Exchange
+run_tase                  = True    # Tel Aviv Stock Exchange
 run_nsr                   = False   # NASDAQ100+S&P500+RUSSEL1000
-run_all                   = True   # All Nasdaq Stocks
+run_all                   = False   # All Nasdaq Stocks
 run_six                   = False    # All Swiss Stocks
 run_st                    = False    # All (Stockholm) Swedish Stocks
 research_mode             = True     # Research Mode
@@ -49,20 +49,19 @@ automatic_results_folder_selection = False
 # After 1st ever Run: Recommended to use reference (filter and damper)
 # The research mode shall run on new_run as input (new_run >= reference_run) where > means newer
 reference_run_custom = None  # 'Results/Custom/20211109-235448_Bdb_nRes271_Custom'
-reference_run_tase   = 'Results/Tase/20211105-113630_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes291'
+reference_run_tase   = 'Results/Tase/20211112-081706_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes280'
 reference_run_nsr    = 'Results/Nsr/20211106-170744_Tchnlgy3.0_FnnclSrvcs0.75_Bdb_nRes788'
 reference_run_all    = 'Results/All/20211107-151952_Tchnlgy3.0_FnnclSrvcs0.75_A_Bdb_nRes3004'
 reference_run_six    = 'Results/Six/20211012-141034_S_Bdb_nRes30'
 reference_run_st     = 'Results/St/20210915-023602_St_Bdb_nRes130'
 
 new_run_custom = 'Results/Custom/20210917-201728_Bdb_nRes312_Custom'
-new_run_tase   = 'Results/Tase/20211105-113630_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes291'
+new_run_tase   = 'Results/Tase/20211112-081706_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes280'
 new_run_nsr    = 'Results/Nsr/20211106-170744_Tchnlgy3.0_FnnclSrvcs0.75_Bdb_nRes788'
 new_run_all    = 'Results/All/20211111-172015_Tchnlgy3.0_FnnclSrvcs0.75_A_Bdb_nRes2934'
 new_run_six    = 'Results/Six/20211012-141034_S_Bdb_nRes30'
 new_run_st     = 'Results/St/20210915-023602_St_Bdb_nRes130'
 
-# TODO: ASAFR: Why is debt_to_equity_effective_used    <= 0   a common failure to profitable stocks? investigate loss of data from yfinance... All of those below seem to be false alarms:
 custom_portfolio      = ['SKM']
 custom_portfolio_tase = ['APLP', 'ITMR']
 
