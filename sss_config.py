4cdaf63442bd289e0d70fac0c98860b8fbe9ff0f
asafravid <asaf.rvd@gmail.com>
2021-12-19 00:27:58 +0200

    - Update TASE Official

--- sss_config.py
+++ sss_config.py
@@ -38,7 +38,7 @@ research_mode             = True     # Research Mode
 aggregate_only            = False
 research_mode_max_ev      = False   # @JustLearning's suggestion in Telegram: Multi-Dimensional Scan by Max EV Limit rather than Min EV Limit
 use_reference_as_raw_data = False
-custom_sss_value_equation = True
+custom_sss_value_equation = False
 
 # When automatic_results_folder_selection is False, the explicitly specified paths below are used for the
 # reference and new_run folder locations.
@@ -50,14 +50,14 @@ automatic_results_folder_selection = False
 # After 1st ever Run: Recommended to use reference (filter and damper)
 # The research mode shall run on new_run as input (new_run >= reference_run) where > means newer
 reference_run_custom = None  # 'Results/Custom/20211109-235448_Bdb_nRes271_Custom'
-reference_run_tase   = 'Results/Tase/20211211-182250_Tase_Tchnlgy3.0_RlEstt1.0_nRes280_CustSssV'  # '20211204-190025_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes280_CustSssV'
+reference_run_tase   = 'Results/Tase/20211217-103246_Tase_Tchnlgy3.0_RlEstt1.0_nRes273' #  '20211211-182250_Tase_Tchnlgy3.0_RlEstt1.0_nRes280_CustSssV'  # '20211204-190025_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes280_CustSssV'
 reference_run_nsr    = 'Results/Nsr/20211125-171810_Tchnlgy3.0_FnnclSrvcs0.75_Bdb_nRes775_CustSssV'
 reference_run_all    = 'Results/All/20211206-170854_Tchnlgy3.0_FnnclSrvcs0.75_A_Bdb_nRes2960'
 reference_run_six    = 'Results/Six/20211216-001037_S_nRes28'                                        # '20211216-002301_S_nRes27_CustSssV'
 reference_run_st     = 'Results/St/20210915-023602_St_Bdb_nRes130'
 
 new_run_custom       = 'Results/Custom/20210917-201728_Bdb_nRes312_Custom'
-new_run_tase         = 'Results/Tase/20211217-150412_Tase_Tchnlgy3.0_RlEstt1.0_nRes276_CustSssV'
+new_run_tase         = 'Results/Tase/20211217-103246_Tase_Tchnlgy3.0_RlEstt1.0_nRes273'
 new_run_nsr          = 'Results/Nsr/20211205-011557_Tchnlgy3.0_FnnclSrvcs0.75_Bdb_nRes775_CustSssV'
 new_run_all          = 'Results/All/20211206-170854_Tchnlgy3.0_FnnclSrvcs0.75_A_Bdb_nRes2960'
 new_run_six          = 'Results/Six/20211216-001037_S_nRes28'                                        # '20211216-002301_S_nRes27_CustSssV'
