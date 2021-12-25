85bf318d5ac530d2d4e06b9adb9b23417ae132b7
Asaf Ravid <aravid@marvell.com>
2021-12-25 22:31:33 +0200

    - Update TASE Light Results for 20211224

--- sss_config.py
+++ sss_config.py
@@ -38,7 +38,7 @@ research_mode             = True   # Research Mode
 aggregate_only            = False
 research_mode_max_ev      = False   # @JustLearning's suggestion in Telegram: Multi-Dimensional Scan by Max EV Limit rather than Min EV Limit
 use_reference_as_raw_data = False
-custom_sss_value_equation = False
+custom_sss_value_equation = True
 
 # When automatic_results_folder_selection is False, the explicitly specified paths below are used for the
 # reference and new_run folder locations.
@@ -50,14 +50,14 @@ automatic_results_folder_selection = False
 # After 1st ever Run: Recommended to use reference (filter and damper)
 # The research mode shall run on new_run as input (new_run >= reference_run) where > means newer
 reference_run_custom = None  # 'Results/Custom/20211109-235448_Bdb_nRes271_Custom'
-reference_run_tase   = 'Results/Tase/20211224-103922_Tase_Tchnlgy7.0_RlEstt1.0_nRes283'  #  '20211211-182250_Tase_Tchnlgy3.0_RlEstt1.0_nRes280_CustSssV'  # '20211204-190025_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes280_CustSssV'
+reference_run_tase   = 'Results/Tase/20211219-112825_Tase_Tchnlgy3.0_RlEstt1.0_nRes276_CustSssV'  # 20211224-103922_Tase_Tchnlgy7.0_RlEstt1.0_nRes283'  #  '20211211-182250_Tase_Tchnlgy3.0_RlEstt1.0_nRes280_CustSssV'  # '20211204-190025_Tase_Tchnlgy3.0_RlEstt1.0_Bdb_nRes280_CustSssV'
 reference_run_nsr    = 'Results/Nsr/20211205-011557_Tchnlgy3.0_FnnclSrvcs0.75_Bdb_nRes775_CustSssV'
 reference_run_all    = None  # 'Results/All/20211206-202021_Tchnlgy3.0_FnnclSrvcs0.75_A_Bdb_nRes2958_CustSssV'
 reference_run_six    = 'Results/Six/20211216-001037_S_nRes28'                                        # '20211216-002301_S_nRes27_CustSssV'
 reference_run_st     = 'Results/St/20210915-023602_St_Bdb_nRes130'
 
 new_run_custom       = 'Results/Custom/20210917-201728_Bdb_nRes312_Custom'
-new_run_tase         = 'Results/Tase/20211224-103922_Tase_Tchnlgy7.0_RlEstt1.0_nRes283'
+new_run_tase         = 'Results/Tase/20211225-222448_Tase_Tchnlgy7.0_RlEstt1.0_nRes281_CustSssV'  # 20211224-103922_Tase_Tchnlgy7.0_RlEstt1.0_nRes283'
 new_run_nsr          = 'Results/Nsr/20211220-002530_Tchnlgy3.0_FnnclSrvcs0.75_nRes781_CustSssV'
 new_run_all          = 'Results/All/20211206-202021_Tchnlgy3.0_FnnclSrvcs0.75_A_Bdb_nRes2958_CustSssV'
 new_run_six          = 'Results/Six/20211216-001037_S_nRes28'                                        # '20211216-002301_S_nRes27_CustSssV'
