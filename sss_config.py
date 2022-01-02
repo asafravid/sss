ee548304a1ff2ea362f7951026371e725bb03b8a
asafravid <61Helo61Helo>
2022-01-02 18:26:09 +0200

    - Add All Scan Results 20220101

--- sss_config.py
+++ sss_config.py
@@ -30,11 +30,11 @@ ALL_COUNTRY_SYMBOLS_ST  = 3  # Swedish (Stockholm) Stock Exchange
 run_custom_tase           = False    # Custom Portfolio
 run_custom                = False
 run_tase                  = False   # Tel Aviv Stock Exchange
-run_nsr                   = True   # NASDAQ100+S&P500+RUSSEL1000
-run_all                   = False   # All Nasdaq Stocks
+run_nsr                   = False   # NASDAQ100+S&P500+RUSSEL1000
+run_all                   = True   # All Nasdaq Stocks
 run_six                   = False   # All Swiss Stocks
 run_st                    = False   # All (Stockholm) Swedish Stocks
-research_mode             = False   # Research Mode
+research_mode             = True   # Research Mode
 aggregate_only            = False
 research_mode_max_ev      = False   # @JustLearning's suggestion in Telegram: Multi-Dimensional Scan by Max EV Limit rather than Min EV Limit
 use_reference_as_raw_data = False
@@ -52,18 +52,18 @@ automatic_results_folder_selection = False
 reference_run_custom = None  # 'Results/Custom/20211109-235448_Bdb_nRes271_Custom'
 reference_run_tase   = 'Results/Tase/20211231-084025_Tase_Tchnlgy7.0_RlEstt1.0_nRes280'
 reference_run_nsr    = 'Results/Nsr/20211231-140343_Tchnlgy7.0_FnnclSrvcs1.0_nRes773'
-reference_run_all    = 'Results/All/20211229-182539_Tchnlgy7.0_FnnclSrvcs1.0_A_nRes2952'  # 20211219-232552_Tchnlgy3.0_FnnclSrvcs0.75_A_nRes2957'
+reference_run_all    = 'Results/All/20220102-020200_Tchnlgy7.0_FnnclSrvcs1.0_A_nRes2950'
 reference_run_six    = 'Results/Six/20211216-001037_S_nRes28'                                        # '20211216-002301_S_nRes27_CustSssV'
 reference_run_st     = 'Results/St/20210915-023602_St_Bdb_nRes130'
 
 new_run_custom       = 'Results/Custom/20210917-201728_Bdb_nRes312_Custom'
 new_run_tase         = 'Results/Tase/20211231-084025_Tase_Tchnlgy7.0_RlEstt1.0_nRes280'
 new_run_nsr          = 'Results/Nsr/20211231-140343_Tchnlgy7.0_FnnclSrvcs1.0_nRes773'
-new_run_all          = 'Results/All/20211206-202021_Tchnlgy3.0_FnnclSrvcs0.75_A_Bdb_nRes2958_CustSssV'
+new_run_all          = 'Results/All/20220102-020200_Tchnlgy7.0_FnnclSrvcs1.0_A_nRes2950'
 new_run_six          = 'Results/Six/20211216-001037_S_nRes28'                                        # '20211216-002301_S_nRes27_CustSssV'
 new_run_st           = 'Results/St/20210915-023602_St_Bdb_nRes130'
 
-crash_and_continue_path = None # 'Results/Nsr/20211207-215612_Tchnlgy3.0_FnnclSrvcs0.75_cc'
+crash_and_continue_path = 'Results/All/20211231-103908_Tchnlgy7.0_FnnclSrvcs1.0_A_cc'
 
 custom_portfolio      = ['AAPL', 'AB', 'ABBV', 'ABC', 'ABEV', 'ACGL', 'ACN', 'ADM', 'AEF', 'AFL', 'AGIO', 'AGO', 'AKAM', 'AL', 'ALGN', 'ALL', 'ALLY', 'AM', 'AMAT', 'AMCR', 'AMD']
 custom_portfolio_tase = ['APLP', 'ITMR']
