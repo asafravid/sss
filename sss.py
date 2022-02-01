21812e4c6083ff5834217c9d0ffc87f08efa3958
asafravid <asaf.rvd@gmail.com>
2022-02-02 00:38:16 +0200

    - Add TODOs for https://github.com/portfolioplus/pysymbolscanner usage

--- sss.py
+++ sss.py
@@ -24,6 +24,7 @@
 # TODO: ASAFR: -1. In the re-run (reference_raw_data) - use the exact indices as used previously in the actual raw data creation - to avoid mishaps
 #              -2. In the reference raw data run - allow configurability to DO the scan of download values... for the sake of clarity
 #              -3. In the MA columns diff - identify changes and get the falling back on track somehow...
+#              -4. Add https://github.com/portfolioplus/pytickersymbols support/usage along with https://github.com/portfolioplus/pysymbolscanner
 #               0. Auto-Update Nasdaq (NSR) Indices as done with TASE
 #               0.1. Add support for https://simfin.com/data/bulk
 #               1.2. https://en.wikipedia.org/wiki/Magic_formula_investing
