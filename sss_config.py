08a33005abed4aa08ab75b6585ad90a990dafe0b
asafravid <asaf.rvd@gmail.com>
2022-02-19 15:32:54 +0200

    - Add 20220219 TASE Light Results

--- sss_config.py
+++ sss_config.py
@@ -37,8 +37,8 @@ run_st                    = False  # All (Stockholm) Swedish Stocks
 research_mode             = True   # Research Mode
 aggregate_only            = False
 research_mode_max_ev      = False  # @JustLearning's suggestion in Telegram: Multi-Dimensional Scan by Max EV Limit rather than Min EV Limit
-use_reference_as_raw_data = False
-custom_sss_value_equation = False
+use_reference_as_raw_data = True
+custom_sss_value_equation = True
 
 scan_close_values_interval = '1d'
 
@@ -52,14 +52,14 @@ automatic_results_folder_selection = False
 # After 1st ever Run: Recommended to use reference (filter and damper)
 # The research mode shall run on new_run as input (new_run >= reference_run) where > means newer
 reference_run_custom = None  # 'Results/Custom/20211109-235448_Bdb_nRes271_Custom'
-reference_run_tase   = 'Results/Tase/20220211-092435_Tase_Tchnlgy7.0_RlEstt1.0_nRes272'  # 20220204-100749_Tase_Tchnlgy7.0_RlEstt1.0_nRes272_CustSssV'  #
+reference_run_tase   = 'Results/Tase/20220211-210900_Tase_Tchnlgy7.0_RlEstt1.0_nRes270_CustSssV'  # 20220218-103932_Tase_Tchnlgy7.0_RlEstt1.0_nRes271'
 reference_run_nsr    = 'Results/Nsr/20220129-230539_Tchnlgy7.0_FnnclSrvcs1.0_nRes792_CustSssV'  # 20220212-143145_Tchnlgy7.0_FnnclSrvcs1.0_nRes795'
 reference_run_all    = 'Results/All/20220131-141318_Tchnlgy7.0_FnnclSrvcs1.0_A_nRes3000_CustSssV'  # 20220214-060241_Tchnlgy7.0_FnnclSrvcs1.0_A_nRes3021'
 reference_run_six    = 'Results/Six/20220111-002719_S_nRes196'                                        # '20211216-002301_S_nRes27_CustSssV'
 reference_run_st     = 'Results/St/20210915-023602_St_Bdb_nRes130'
 
 new_run_custom       = 'Results/Custom/20210917-201728_Bdb_nRes312_Custom'
-new_run_tase         = 'Results/Tase/20220218-103932_Tase_Tchnlgy7.0_RlEstt1.0_nRes271'  # 20220211-210900_Tase_Tchnlgy7.0_RlEstt1.0_nRes270_CustSssV'
+new_run_tase         = 'Results/Tase/20220219-124717_Tase_Tchnlgy7.0_RlEstt1.0_nRes269_CustSssV'  # 20220218-103932_Tase_Tchnlgy7.0_RlEstt1.0_nRes271'  # 20220211-210900_Tase_Tchnlgy7.0_RlEstt1.0_nRes270_CustSssV'
 new_run_nsr          = 'Results/Nsr/20220212-180523_Tchnlgy7.0_FnnclSrvcs1.0_nRes793_CustSssV'  # 20220212-143145_Tchnlgy7.0_FnnclSrvcs1.0_nRes795'
 new_run_all          = 'Results/All/20220215-195325_Tchnlgy7.0_FnnclSrvcs1.0_A_nRes3005_CustSssV'  # 20220214-060241_Tchnlgy7.0_FnnclSrvcs1.0_A_nRes3021'
 new_run_six          = 'Results/Six/20220111-002719_S_nRes196'                                        # '20211216-002301_S_nRes27_CustSssV'
