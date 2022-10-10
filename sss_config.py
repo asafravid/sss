d395bd14d64e44171497a6c30169f893a8ac7628
Asaf Ravid <asaf.rvd@gmail.com>
2022-10-10 07:58:22 +0300

    - TASE OCT 2022

--- sss_config.py
+++ sss_config.py
@@ -29,16 +29,16 @@ ALL_COUNTRY_SYMBOLS_ST  = 3  # Swedish (Stockholm) Stock Exchange
 
 run_custom_tase           = False  # Custom Portfolio
 run_custom                = False
-run_tase                  = False  # Tel Aviv Stock Exchange
+run_tase                  = True   # Tel Aviv Stock Exchange
 run_nsr                   = False  # NASDAQ100+S&P500+RUSSEL1000
-run_all                   = True   # All Nasdaq Stocks
+run_all                   = False  # All Nasdaq Stocks
 run_six                   = False  # All Swiss Stocks
 run_st                    = False  # All (Stockholm) Swedish Stocks
-multi_dim_scan_mode       = True   # Mult-dimentional scan Mode
+multi_dim_scan_mode       = True    # Mult-dimentional scan Mode
 aggregate_only            = False
 research_mode_max_ev      = False
-use_reference_as_raw_data = False
-custom_sss_value_equation = False
+use_reference_as_raw_data = True
+custom_sss_value_equation = True
 
 scan_close_values_interval      = '1d'
 crash_and_continue_refresh_freq = 10 if run_custom or run_custom_tase else 100 # Flush every 100 symbols (or less for custom)
@@ -53,14 +53,14 @@ automatic_results_folder_selection = False
 # After 1st ever Run: Recommended to use reference (filter and damper)
 # The research mode shall run on new_run as input (new_run >= reference_run) where > means newer
 reference_run_custom = 'Results/All/20220419-020633_A_nRes112_Custom_CustSssV'
-reference_run_tase   = 'Results/Tase/20220714-183655_Tase_Tchnlgy3.0_RlEstt1.0_nRes293'
+reference_run_tase   = 'Results/Tase/20220714-203656_Tase_Tchnlgy3.0_RlEstt1.0_nRes293_CustSssV'
 reference_run_nsr    = 'Results/Nsr/20220605-221926_Tchnlgy3.0_FnnclSrvcs1.0_nRes837'
 reference_run_all    = 'Results/All/20220507-011303_Tchnlgy7.0_FnnclSrvcs1.0_A_nRes2920'
 reference_run_six    = 'Results/Six/20220111-002719_S_nRes196'                                        # '20211216-002301_S_nRes27_CustSssV'
 reference_run_st     = 'Results/St/20210915-023602_St_Bdb_nRes130'
 
 new_run_custom       = 'Results/Custom/20210917-201728_Bdb_nRes312_Custom'
-new_run_tase         = 'Results/Tase/20220822-124035_Tase_Tchnlgy3.0_RlEstt1.0_nRes298'
+new_run_tase         = 'Results/Tase/20221010-075445_Tase_Tchnlgy3.0_RlEstt1.0_nRes299_CustSssV'
 new_run_nsr          = 'Results/Nsr/20220823-043826_Tchnlgy3.0_FnnclSrvcs1.0_nRes837'
 new_run_all          = 'Results/All/20220827-133324_Tchnlgy3.0_FnnclSrvcs1.0_A_nRes2980'
 new_run_six          = 'Results/Six/20220111-002719_S_nRes196'                                        # '20211216-002301_S_nRes27_CustSssV'
